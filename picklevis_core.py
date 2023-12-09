0ed48dbe8966d950de592645bd7bb7f94a4e925e
Inoki <veyx.shaw@gmail.com>
2023-12-09 13:25:13 +0100

    Add print for opcodes count

--- picklevis_core.py
+++ picklevis_core.py
@@ -21,6 +21,7 @@ class Unpickler(pickle._Unpickler):
     def __init__(self, file, *args, **kwargs) -> None:
         super().__init__(file, *args, **kwargs)
 
+        self.picklevis_opcodes = []
         self.picklevis_ops = []
         self.picklevis_byte_count = []
         self._file = file
@@ -32,6 +33,7 @@ class Unpickler(pickle._Unpickler):
         def inspector(self, *args, **kwargs):
             logger.debug(f"Calling {func.__name__} for opcode 0x{opcode:02x}")
             self.picklevis_ops.append(func.__name__)
+            self.picklevis_opcodes.append(opcode)
 
             in_frame = False
             if self._unframer is not None and self._unframer.current_frame is not None:
@@ -62,6 +64,7 @@ if __name__ == "__main__":
     with open("data.pkl", "rb") as f:
         unpickler = Unpickler(f)
         unpickler.load()
-        print(f"OPCODES:\t{len(unpickler.picklevis_ops)}")
+        print(f"OPERATIONS:\t{len(unpickler.picklevis_ops)}")
+        print(f"OPCODES:\t{len(unpickler.picklevis_opcodes)}")
         print(f"READ BYTES:\t{sum(unpickler.picklevis_byte_count)}")
 
