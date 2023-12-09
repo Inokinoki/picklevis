8c8789c92a9f9c5bb7af715dfcae4ec6b30d9ebf
Inoki <veyx.shaw@gmail.com>
2023-12-09 14:54:46 +0100

    Add mapping from int opcode to name

--- picklevis_core.py
+++ picklevis_core.py
@@ -7,13 +7,15 @@ logger = logging.getLogger(__file__)
 
 OPCODES = pickle._Unpickler.dispatch.keys()
 
-OPCODE_NAME_MAPPING = {}    # A bytes (with only one byte) to opcode name string mapping
+OPCODE_NAME_MAPPING = {}        # A bytes (with only one byte) to opcode name string mapping
+OPCODE_INT_NAME_MAPPING = {}    # An int to opcode name string mapping
 pickle_objects_dict = pickle.__dict__
 for k in pickle_objects_dict:
     pickle_object = pickle_objects_dict[k]
     if isinstance(pickle_object, bytes) and len(pickle_object) == 1:
         if int(pickle_object[0]) in OPCODES:
             OPCODE_NAME_MAPPING[pickle_object] = k
+            OPCODE_INT_NAME_MAPPING[int(pickle_object[0])] = k
 
 
 class Unpickler(pickle._Unpickler):
