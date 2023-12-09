0a11a8c84ec9c1a6642a5dcf7d7fab617d235d81
Inoki <veyx.shaw@gmail.com>
2023-12-09 12:43:49 +0100

    Add basic opcode inspection

--- picklevis_core.py
+++ picklevis_core.py
@@ -1,4 +1,9 @@
 import pickle
+from typing import Callable
+import logging
+
+
+logger = logging.getLogger(__file__)
 
 OPCODES = pickle._Unpickler.dispatch.keys()
 
@@ -9,3 +14,23 @@ for k in pickle_objects_dict:
     if isinstance(pickle_object, bytes) and len(pickle_object) == 1:
         if int(pickle_object[0]) in OPCODES:
             OPCODE_NAME_MAPPING[pickle_object] = k
+
+
+class Unpickler(pickle._Unpickler):
+
+    def __init__(self, file, *args, **kwargs) -> None:
+        super().__init__(file, *args, **kwargs)
+        for opcode in OPCODES:
+            self.dispatch[opcode] = self.inspect_dispatch(opcode, self.dispatch[opcode])
+
+    def inspect_dispatch(self, opcode, func: Callable):
+        def inspector(self, *args, **kwargs):
+            logger.info(f"Calling {func.__name__} for opcode 0x{opcode:02x}")
+            func(self, *args, **kwargs)
+
+        return inspector
+
+
+if __name__ == "__main__":
+    with open("data.pkl", "rb") as f:
+        Unpickler(f).load()
